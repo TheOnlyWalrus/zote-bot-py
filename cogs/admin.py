b8e0abb80aca27999b80c64bd83cd04e53807945
Wynn <theonlywalrus01@gmail.com>
2022-05-04 04:03:42 -0600

    eval

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,13 +1,11 @@
+import ast
 import asyncio
 import copy
 import discord
-import inspect
-import io
 import subprocess
 import traceback
 import typing
 
-from contextlib import redirect_stdout
 from discord.ext import commands
 from utils.checks import access_level, AccessLevel, access_level_cog
 
@@ -46,94 +44,50 @@ class Admin(commands.Cog):
             return f'```py\n{e.__class__.__name__}: {e}\n```'
         return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
 
+    def insert_returns(self, body):
+        if isinstance(body[-1], ast.Expr):
+            body[-1] = ast.Return(body[-1].value)
+            ast.fix_missing_locations(body[-1])
+
+        if isinstance(body[-1], ast.If):
+            self.insert_returns(body[-1].body)
+            self.insert_returns(body[-1].orelse)
+
+        if isinstance(body[-1], ast.With):
+            self.insert_returns(body[-1].body)
+
     @commands.command()
     @access_level(AccessLevel.ADMIN)
-    async def repl(self, ctx):
-        """Launches an interactive REPL session."""
-        variables = {
+    async def eval(self, ctx, *, cmd):
+        fn_name = "_eval_expr"
+
+        if cmd.startswith("```") and cmd.endswith("```"):
+            cmd = cmd[3:-3]
+        elif cmd.startswith("`") and cmd.endswith("`"):
+            cmd = cmd[1:-1]
+
+        cmd = "\n".join(f"    {i}" for i in cmd.splitlines())
+
+        body = f"async def {fn_name}():\n{cmd}"
+
+        parsed = ast.parse(body)
+        body = parsed.body[0].body
+
+        self.insert_returns(body)
+
+        env = {
+            'bot': ctx.bot,
+            'discord': discord,
+            'commands': commands,
             'ctx': ctx,
-            'bot': self.bot,
-            'message': ctx.message,
-            'guild': ctx.guild,
-            'channel': ctx.channel,
-            'author': ctx.author,
-            '_': None,
+            'asyncio': asyncio,
+            '__import__': __import__
         }
 
-        if ctx.channel.id in self.sessions:
-            await ctx.send('Already running a REPL session in this channel. Exit it with `quit`.')
-            return
+        exec(compile(parsed, filename="<ast>", mode="exec"), env)
 
-        self.sessions.add(ctx.channel.id)
-        await ctx.send('Enter code to execute or evaluate. `exit()` or `quit` to exit.')
-
-        def check(m):
-            return m.author.id == ctx.author.id and m.channel.id == ctx.channel.id and m.content.startswith('`')
-
-        while True:
-            try:
-                response = await self.bot.wait_for('message', check=check, timeout=10.0 * 60.0)
-            except asyncio.TimeoutError:
-                await ctx.send('Exiting REPL session.')
-                self.sessions.remove(ctx.channel.id)
-                break
-
-            cleaned = self.cleanup_code(response.content)
-
-            if cleaned in ('quit', 'exit', 'exit()'):
-                await ctx.send('Exiting.')
-                self.sessions.remove(ctx.channel.id)
-                return
-
-            executor = exec
-            if cleaned.count('\n') == 0:
-                # single statement, potentially 'eval'
-                try:
-                    code = compile(cleaned, '<repl session>', 'eval')
-                except SyntaxError:
-                    pass
-                else:
-                    executor = eval
-
-            if executor is exec:
-                try:
-                    code = compile(cleaned, '<repl session>', 'exec')
-                except SyntaxError as e:
-                    await ctx.send(self.get_syntax_error(e))
-                    continue
-
-            variables['message'] = response
-
-            fmt = None
-            stdout = io.StringIO()
-            code = ''
-
-            try:
-                with redirect_stdout(stdout):
-                    result = executor(code, variables)
-                    if inspect.isawaitable(result):
-                        result = await result
-            except Exception as e:
-                value = stdout.getvalue()
-                fmt = f'```py\n{value}{traceback.format_exc()}\n```'
-            else:
-                value = stdout.getvalue()
-                if result is not None:
-                    fmt = f'```py\n{value}{result}\n```'
-                    variables['_'] = result
-                elif value:
-                    fmt = f'```py\n{value}\n```'
-
-            try:
-                if fmt is not None:
-                    if len(fmt) > 2000:
-                        await ctx.send('Content too big to be printed.')
-                    else:
-                        await ctx.send(fmt)
-            except discord.Forbidden:
-                pass
-            except discord.HTTPException as e:
-                await ctx.send(f'Unexpected error: `{e}`')
+        ret = await eval(f"{fn_name}()", env)
+        await ctx.send(ret)
 
     @commands.command()
     @access_level(AccessLevel.ADMIN)
