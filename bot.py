9885503bd8fed7cf98626e49b49065fe458cca32
Wynn <theonlywalrus01@gmail.com>
2022-05-14 22:54:32 -0600

    Moved some stuff around

--- bot.py
+++ bot.py
@@ -6,22 +6,12 @@ from discord.ext import commands
 from utils.database import DBConnection
 from utils.utils import aloc
 
-logger = logging.getLogger(
-    'zote' if os.environ.get('DEBUG', "0") == "0"
-    else 'purple'
-)
-logger.setLevel(logging.DEBUG)
-
-handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
 
 class ZoteBot(commands.AutoShardedBot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.db = DBConnection()
+        self._watcher_mode = os.environ.get('WATCHER_MODE', "0") == "1"
 
     async def close(self):
         if self.db.is_connected:
@@ -29,24 +19,38 @@ class ZoteBot(commands.AutoShardedBot):
 
         await super().close()
 
+    async def on_message(self, message):
+        if not self._watcher_mode:
+            await self.process_commands(message)
 
-bot = ZoteBot(command_prefix='>', intents=discord.Intents.all())  # TODO: Change prefix back to > when done testing
-
-
-for filename in os.listdir('cogs'):
-    if filename.endswith('.py'):
-        try:
-            bot.load_extension(f'cogs.{filename[:-3]}')
-        except commands.ExtensionError as e:
-            logger.error(e)
+    async def on_ready(self):
+        await self.db.connect()
+        logger.debug(f'Logged in as {bot.user} ({bot.user.id})')
+        logger.debug(f'All lines of code: {aloc()}')
 
 
-@bot.event
-async def on_ready():
-    logger.debug(f'Logged in as {bot.user} ({bot.user.id})')
-    logger.debug(f'All lines of code: {aloc()}')
+if __name__ == '__main__':
+    logger = logging.getLogger(
+        'zote' if os.environ.get('DEBUG', "0") == "0"
+        else 'purple'
+    )
+    logger.setLevel(logging.DEBUG)
 
-bot.run(
-    os.environ.get('ZOTE_DISCORD_TOKEN', 'abcdefg1234567') if os.environ.get('DEBUG', "0") == "0"
-    else os.environ.get('PURPLE_DISCORD_TOKEN', 'abcdefg1234567')
-)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    bot = ZoteBot(command_prefix='>', intents=discord.Intents.all())
+
+    for filename in os.listdir('cogs'):
+        if filename.endswith('.py'):
+            try:
+                bot.load_extension(f'cogs.{filename[:-3]}')
+            except commands.ExtensionError as e:
+                logger.error(e)
+
+    bot.run(
+        os.environ.get('ZOTE_DISCORD_TOKEN', 'abcdefg1234567') if os.environ.get('DEBUG', "0") == "0"
+        else os.environ.get('PURPLE_DISCORD_TOKEN', 'abcdefg1234567')
+    )
